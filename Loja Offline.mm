e6ef702278ad4604a4119485fc8a97c675640589
ROLL Software Brasil <rogerioorkutmsn@hotmail.com>
2017-05-01 23:47:59 -0300

    Update: arquivo mapa mental
    Mapa mental do projeto.

---
+++ Loja Offline.mm
@@ -0,0 +1,474 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Sistema de Loja" FOLDED="false" ID="ID_842804500" CREATED="1493413743752" MODIFIED="1493415371533" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="0.829">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Requisitos" POSITION="right" ID="ID_1413209035" CREATED="1493415283577" MODIFIED="1493690646929">
+<edge COLOR="#ff0000"/>
+<richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      120h (previs&#227;o inicial)
+    </p>
+    <p>
+      240h (previs&#227;o inicial - com testes)
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Netbeans
+    </p>
+    <p>
+      Java SE
+    </p>
+    <p>
+      MySQL Workbench
+    </p>
+    <p>
+      SQL
+    </p>
+    <p>
+      Diagramas de Classe UML DBDesigner
+    </p>
+    <p>
+      Git
+    </p>
+    <p>
+      Freeplane
+    </p>
+    <p>
+      JUnit
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Aprender
+    </p>
+    <p>
+      Java+Banco de Dados 12h
+    </p>
+    <p>
+      JBDC/Eclipselink/Hibernate
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Atores" ID="ID_1432924995" CREATED="1493415697498" MODIFIED="1493415700347">
+<node TEXT="Usu&#xe1;rio" ID="ID_865395944" CREATED="1493415708442" MODIFIED="1493415719641">
+<node TEXT="Vendedor" ID="ID_1281174835" CREATED="1493415717018" MODIFIED="1493415726171"/>
+<node TEXT="Administrador" ID="ID_1300701845" CREATED="1493415728123" MODIFIED="1493415732892"/>
+</node>
+<node TEXT="Sistema" ID="ID_1364986360" CREATED="1493416391405" MODIFIED="1493416413562"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Sistema de loja. Offline para desktop.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Produto" ID="ID_1518629356" CREATED="1493414278380" MODIFIED="1493690652710"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      6h (previs&#227;o inicial)
+    </p>
+    <p>
+      6h (previs&#227;o inicial - com testes)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Servi&#xe7;o" ID="ID_586797442" CREATED="1493425864505" MODIFIED="1493690656503"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      6h (previs&#227;o inicial)
+    </p>
+    <p>
+      6h (previs&#227;o inicial - com testes)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Usu&#xe1;rio" ID="ID_27664375" CREATED="1493414367351" MODIFIED="1493690659271"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      6h (previs&#227;o inicial)
+    </p>
+    <p>
+      6h (previs&#227;o inicial - com testes)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Administrador" ID="ID_1614254843" CREATED="1493432560023" MODIFIED="1493690662199"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      6h (previs&#227;o inicial)
+    </p>
+    <p>
+      6h (previs&#227;o inicial - com testes)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Vendedor" ID="ID_1228494905" CREATED="1493432570743" MODIFIED="1493690664985"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      6h (previs&#227;o inicial)
+    </p>
+    <p>
+      6h (previs&#227;o inicial - com testes)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Cliente" ID="ID_1847981504" CREATED="1493416473998" MODIFIED="1493690668071"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      6h (previs&#227;o inicial)
+    </p>
+    <p>
+      6h (previs&#227;o inicial - com testes)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Fornecedor" ID="ID_1934583146" CREATED="1493417161712" MODIFIED="1493690671175"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      6h (previs&#227;o inicial)
+    </p>
+    <p>
+      6h (previs&#227;o inicial - com testes)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Invent&#xe1;rio" ID="ID_1075254580" CREATED="1493429530474" MODIFIED="1493690676715"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      6h (previs&#227;o inicial)
+    </p>
+    <p>
+      6h (previs&#227;o inicial - com testes)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Estoque" ID="ID_1387543511" CREATED="1493414493678" MODIFIED="1493690674407"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      6h (previs&#227;o inicial)
+    </p>
+    <p>
+      6h (previs&#227;o inicial - com testes)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Venda" ID="ID_561706489" CREATED="1493414504381" MODIFIED="1493690679000"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      6h (previs&#227;o inicial)
+    </p>
+    <p>
+      6h (previs&#227;o inicial - com testes)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Compra" ID="ID_1481853501" CREATED="1493414511966" MODIFIED="1493690681193"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      6h (previs&#227;o inicial)
+    </p>
+    <p>
+      6h (previs&#227;o inicial - com testes)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="Cen&#xe1;rio" ID="ID_1259744657" CREATED="1493415702171" MODIFIED="1493415705275">
+<node TEXT="Vendedor" ID="ID_550769796" CREATED="1493418542555" MODIFIED="1493418547820">
+<node TEXT="Produto" FOLDED="true" ID="ID_236988710" CREATED="1493430186810" MODIFIED="1493430189419">
+<node TEXT="Cadastrar Produto" ID="ID_549861491" CREATED="1493418952822" MODIFIED="1493418957992"/>
+<node TEXT="Editar Produto" ID="ID_1405088348" CREATED="1493419722655" MODIFIED="1493422605069"/>
+<node TEXT="Consultar Produto" ID="ID_772100629" CREATED="1493419471838" MODIFIED="1493419479407"/>
+</node>
+<node TEXT="Servi&#xe7;o" FOLDED="true" ID="ID_1419471291" CREATED="1493430413146" MODIFIED="1493430415932">
+<node TEXT="Cadastrar Servi&#xe7;o" ID="ID_1989814830" CREATED="1493429121582" MODIFIED="1493429133647"/>
+<node TEXT="Editar Servi&#xe7;o" ID="ID_1988766804" CREATED="1493429135454" MODIFIED="1493429139232"/>
+<node TEXT="Consultar Servi&#xe7;o" ID="ID_597195014" CREATED="1493429140702" MODIFIED="1493429145599"/>
+</node>
+<node TEXT="Cliente" FOLDED="true" ID="ID_1386358623" CREATED="1493430399306" MODIFIED="1493430400860">
+<node TEXT="Cadastrar Cliente" ID="ID_1316367097" CREATED="1493418961175" MODIFIED="1493418966983"/>
+<node TEXT="Editar Cliente" ID="ID_365500223" CREATED="1493420103364" MODIFIED="1493430402705"/>
+<node TEXT="Consultar Cliente" ID="ID_413378525" CREATED="1493420097684" MODIFIED="1493420101078"/>
+</node>
+<node TEXT="Fornecedor" FOLDED="true" ID="ID_1126930697" CREATED="1493430164634" MODIFIED="1493430172075">
+<node TEXT="Cadastrar Fornecedor" ID="ID_543339152" CREATED="1493418969495" MODIFIED="1493418975255"/>
+<node TEXT="Editar Fornecedor" ID="ID_494280578" CREATED="1493420130532" MODIFIED="1493422585594"/>
+<node TEXT="Consultar Fornecedor" ID="ID_775157066" CREATED="1493422618169" MODIFIED="1493422623402"/>
+</node>
+<node TEXT="Invent&#xe1;rio" FOLDED="true" ID="ID_1868566200" CREATED="1493430962042" MODIFIED="1493430965180">
+<node TEXT="Registrar Invent&#xe1;rio" ID="ID_105569469" CREATED="1493429548474" MODIFIED="1493429936907">
+<node TEXT="Consultar Produto" ID="ID_1648739507" CREATED="1493419471838" MODIFIED="1493419479407"/>
+<node TEXT="Inserir Estoque" ID="ID_1335249398" CREATED="1493423084549" MODIFIED="1493423095479"/>
+<node TEXT="Retirar Estoque" ID="ID_1339060255" CREATED="1493423097893" MODIFIED="1493423105159"/>
+<node TEXT="Cancelar Invent&#xe1;rio" ID="ID_1408325053" CREATED="1493429575290" MODIFIED="1493429580988"/>
+</node>
+<node TEXT="Consultar Invent&#xe1;rio" ID="ID_1130237469" CREATED="1493429541306" MODIFIED="1493430137231"/>
+</node>
+<node TEXT="Compra" FOLDED="true" ID="ID_1901847949" CREATED="1493430931882" MODIFIED="1493430933900">
+<node TEXT="Registrar Compra" ID="ID_1656185467" CREATED="1493423528268" MODIFIED="1493429931903">
+<node TEXT="Fornecedor" ID="ID_1157320477" CREATED="1493430164634" MODIFIED="1493430172075">
+<node TEXT="Cadastrar Fornecedor" ID="ID_1344266177" CREATED="1493418969495" MODIFIED="1493418975255"/>
+<node TEXT="Editar Fornecedor" ID="ID_86893840" CREATED="1493420130532" MODIFIED="1493422585594"/>
+<node TEXT="Consultar Fornecedor" ID="ID_1248811607" CREATED="1493422618169" MODIFIED="1493422623402"/>
+</node>
+<node TEXT="Produto" ID="ID_952135744" CREATED="1493430186810" MODIFIED="1493430189419">
+<node TEXT="Cadastrar Produto" ID="ID_370261808" CREATED="1493418952822" MODIFIED="1493418957992"/>
+<node TEXT="Editar Produto" ID="ID_915314055" CREATED="1493419722655" MODIFIED="1493422605069"/>
+<node TEXT="Consultar Produto" ID="ID_1264600759" CREATED="1493419471838" MODIFIED="1493419479407"/>
+<node TEXT="Inserir Estoque" ID="ID_737811122" CREATED="1493423084549" MODIFIED="1493423095479"/>
+</node>
+<node TEXT="Cancelar Compra" ID="ID_275767491" CREATED="1493423634092" MODIFIED="1493423637485"/>
+</node>
+<node TEXT="Consultar Compra" ID="ID_614058701" CREATED="1493423554012" MODIFIED="1493423596766"/>
+</node>
+<node TEXT="Venda" FOLDED="true" ID="ID_1952030966" CREATED="1493430985866" MODIFIED="1493430987564">
+<node TEXT="Registrar Venda" ID="ID_648512572" CREATED="1493423528268" MODIFIED="1493430402705">
+<node TEXT="Cliente" ID="ID_587591636" CREATED="1493430399306" MODIFIED="1493430400860">
+<node TEXT="Cadastrar Cliente" ID="ID_1624917391" CREATED="1493418961175" MODIFIED="1493418966983"/>
+<node TEXT="Editar Cliente" ID="ID_1545976002" CREATED="1493420103364" MODIFIED="1493430402705"/>
+<node TEXT="Consultar Cliente" ID="ID_824601780" CREATED="1493420097684" MODIFIED="1493420101078"/>
+</node>
+<node TEXT="Produto" ID="ID_321735634" CREATED="1493430446603" MODIFIED="1493430448748">
+<node TEXT="Consultar Produto" ID="ID_11889291" CREATED="1493419471838" MODIFIED="1493419479407"/>
+<node TEXT="Editar Produto" ID="ID_1732475498" CREATED="1493419722655" MODIFIED="1493422605069"/>
+<node TEXT="Consultar Estoque" ID="ID_198779368" CREATED="1493423106997" MODIFIED="1493423117142"/>
+<node TEXT="Retirar Estoque" ID="ID_1297079505" CREATED="1493423097893" MODIFIED="1493423105159"/>
+</node>
+<node TEXT="Servi&#xe7;o" ID="ID_1675314220" CREATED="1493430413146" MODIFIED="1493430415932">
+<node TEXT="Cadastrar Servi&#xe7;o" ID="ID_1016042489" CREATED="1493429121582" MODIFIED="1493429133647"/>
+<node TEXT="Editar Servi&#xe7;o" ID="ID_1498320010" CREATED="1493429135454" MODIFIED="1493429139232"/>
+<node TEXT="Consultar Servi&#xe7;o" ID="ID_998691435" CREATED="1493429140702" MODIFIED="1493429145599"/>
+<node TEXT="Editar Estoque" ID="ID_10853759" CREATED="1493431906618" MODIFIED="1493431957807">
+<node TEXT="Inserir Estoque" ID="ID_1596605622" CREATED="1493432067818" MODIFIED="1493432072427"/>
+<node TEXT="Retirar Estoque" ID="ID_1304695785" CREATED="1493432074602" MODIFIED="1493432083979"/>
+</node>
+</node>
+<node TEXT="Cancelar Venda" ID="ID_576798356" CREATED="1493423627996" MODIFIED="1493423631678"/>
+</node>
+<node TEXT="Consultar Venda" ID="ID_1038671289" CREATED="1493423554012" MODIFIED="1493423561054"/>
+</node>
+</node>
+<node TEXT="Administrador" ID="ID_943489431" CREATED="1493418565403" MODIFIED="1493418569293">
+<node TEXT="Vendedor" FOLDED="true" ID="ID_1774555295" CREATED="1493431025098" MODIFIED="1493431026924">
+<node TEXT="Cadastrar Vendedor" ID="ID_1297919730" CREATED="1493422648857" MODIFIED="1493422652522"/>
+<node TEXT="Consultar Vendedor" ID="ID_694158569" CREATED="1493422642409" MODIFIED="1493422645514"/>
+<node TEXT="Remover Vendedor" ID="ID_1877105523" CREATED="1493420247901" MODIFIED="1493420254175"/>
+</node>
+<node TEXT="Produto" FOLDED="true" ID="ID_1434990622" CREATED="1493431007962" MODIFIED="1493431009932">
+<node TEXT="Consultar Produto" ID="ID_1842085927" CREATED="1493419471838" MODIFIED="1493419479407"/>
+<node TEXT="Remover Produto" ID="ID_1837973002" CREATED="1493420234878" MODIFIED="1493420242703"/>
+</node>
+<node TEXT="Servi&#xe7;o" FOLDED="true" ID="ID_260684132" CREATED="1493431016986" MODIFIED="1493431019148">
+<node TEXT="Consultar Servi&#xe7;o" ID="ID_1384121366" CREATED="1493429140702" MODIFIED="1493429145599"/>
+<node TEXT="Remover Servi&#xe7;o" ID="ID_423013452" CREATED="1493429165614" MODIFIED="1493429171119"/>
+</node>
+<node TEXT="Cliente" FOLDED="true" ID="ID_1193637362" CREATED="1493431040874" MODIFIED="1493431042732">
+<node TEXT="Consultar Cliente" ID="ID_595780798" CREATED="1493420097684" MODIFIED="1493420101078"/>
+<node TEXT="Remover Cliente" ID="ID_780125434" CREATED="1493420255885" MODIFIED="1493420260592"/>
+</node>
+<node TEXT="Fornecedor" FOLDED="true" ID="ID_739851479" CREATED="1493431188213" MODIFIED="1493431191223">
+<node TEXT="Consultar Fornecedor" ID="ID_1561452551" CREATED="1493422618169" MODIFIED="1493422623402"/>
+<node TEXT="Remover Fornecedor" ID="ID_1432840209" CREATED="1493420263549" MODIFIED="1493420268575"/>
+</node>
+<node TEXT="Invent&#xe1;rio" FOLDED="true" ID="ID_1306952158" CREATED="1493431195989" MODIFIED="1493431208039">
+<node TEXT="Consultar Estoque" ID="ID_178724491" CREATED="1493423106997" MODIFIED="1493423117142"/>
+<node TEXT="Remover Estoque" ID="ID_103579946" CREATED="1493423136341" MODIFIED="1493423139302"/>
+</node>
+<node TEXT="Compra" FOLDED="true" ID="ID_332366329" CREATED="1493431268644" MODIFIED="1493431270039">
+<node TEXT="Consultar Compra" ID="ID_603543888" CREATED="1493423554012" MODIFIED="1493423596766"/>
+<node TEXT="Remover Compra" ID="ID_1855366010" CREATED="1493423676620" MODIFIED="1493423680797"/>
+</node>
+<node TEXT="Venda" FOLDED="true" ID="ID_957990362" CREATED="1493431263541" MODIFIED="1493431264918">
+<node TEXT="Consultar Venda" ID="ID_745763443" CREATED="1493423554012" MODIFIED="1493423561054"/>
+<node TEXT="Remover Venda" ID="ID_60675222" CREATED="1493423668012" MODIFIED="1493423674622"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
